3598ac4aefa52a4461c5cc356d40e786b28a6304
bijan <bijan.rudini@gmail.com>
2025-03-14 01:28:08 -0230

    First vectorization attempt

--- Vectorization.py
+++ Vectorization.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
 
 medical_data = pd.read_csv('train.csv')
 
@@ -11,4 +14,16 @@ medical_data['text'] = 'The Question: ' + medical_data['Question'] + '\nExpert r
 
 
 # print(medical_data['text'][10])
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+embeddings = model.encode(medical_data['text'].to_list(), show_progress_bar=True)
+
+embeddings = np.array(embeddings).astype('float32')
+
+index = faiss.IndexFlatL2(embeddings.shape[1])
+index.add(embeddings)
+
+faiss.write_index(index, "Medical_QA.index")
+medical_data.to_csv("Medical_QA.csv", index=False)
 
