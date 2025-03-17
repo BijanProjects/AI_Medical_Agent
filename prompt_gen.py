8bd9eaf1b5937cf644b953db46d6fe282a69a40a
bijan <bijan.rudini@gmail.com>
2025-03-17 19:57:01 -0230

    Modified the prompt and switched deployment to unsloth

--- prompt_gen.py
+++ prompt_gen.py
@@ -1,29 +1,26 @@
 from retriever import similarity_search
-
-context = similarity_search(['Whenever I jump my leg hurts; what should I do?'], 1)
-print(context)
-
-
+import unsloth
+# context = similarity_search(['Whenever I jump my leg hurts; what should I do?'], 1)
+# print(context)
 
 
-# tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
-# model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", device_map='auto')
+
 
 
 prompt = """Instruction:
-You are a specialized AI-powered medical agent trained to provide **clear, structured, and evidence-based** answers to medical questions. Follow these strict rules:
+You are a specialized AI-powered medical agent trained to provide **clear, structured, and evidence-based** answers to medical query at the end. Follow these strict rules:
 
-- **Only answer medical-related questions.** If the user asks a non-medical question, respond with: "I am an AI-powered medical agent and cannot help you with non-medical questions!"
-- **Use ONLY the provided context** to generate your response. **DO NOT make up any medical facts** that are not explicitly stated in the context.
+- **Only answer medical-related queries.** If the user asks a non-medical question, respond with: "I am an AI-powered medical agent and cannot help you with non-medical questions!"
+- **Use ONLY the provided context (questions and answers pairs)** to generate your response. **DO NOT make up any medical facts** that are not explicitly stated in the context.
 - **If information is missing in the context**, state: "I do not have enough information to answer this. Please consult a medical professional."
 - **Do not repeat information unnecessarily.**
-- **If the user asks multiple questions**, structure the response in a step-by-step format.
+- **If the user asks multiple questions**, structure the response in a step-by-step format answering each question accordingly.
 - **Do not mix unrelated medical terms**; only mention medications or treatments that are supported by the provided context.
 
 Context: 
 {}
 
-Question: 
+Query: 
 {}
 
 Response:
